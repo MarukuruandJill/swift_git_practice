b66a9d56e3cfadcc0778285da9b58fb6b54d9a3b
Maria Takei <g2220527is.ocha.ac.jp>
2025-06-10 10:53:45 +0900

    Initial Commit

---
+++ swift_git_practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_git_practice
+//
+//  Created by 武井まりあ on 2025/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_git_practice/swift_git_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_git_practiceApp.swift
+//  swift_git_practice
+//
+//  Created by 武井まりあ on 2025/06/10.
+//
+
+import SwiftUI
+
+@main
+struct swift_git_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
